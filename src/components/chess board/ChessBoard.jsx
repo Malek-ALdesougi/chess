6409cbcec77473b4ac1f2c143c74576b4bf7f5b8
Malek AL-Desougi <malekaldesougi@gmail.com>
2023-05-09 16:20:25 +0300

    first commit

---
+++ src/components/chess board/ChessBoard.jsx
@@ -0,0 +1,11 @@
+import styles from './styel.module.css';
+
+function ChessBoard() {
+  return (
+    <div className={styles.ChessBoard}>
+      <h1>chess board</h1>
+    </div>
+  );
+}
+
+export default ChessBoard;
