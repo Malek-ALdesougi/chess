df3cc9998927549fde0512b5494cbd1fa5c18c8c
Malek AL-Desougi <malekaldesougi@gmail.com>
2023-05-30 12:07:55 +0300

    fix the king moves issue and proceed with stage 2 in checkmate

--- src/functions/AllowedMovesToEscapeCheckMate.js
+++ src/functions/AllowedMovesToEscapeCheckMate.js
@@ -5,6 +5,8 @@ import { checkMovesForSinglePiece } from "./checkMovesSingle";
 const checkMateAllowedMoves = {};
 let kingPossibleMoves = [];
 let kingAllowedMoves = [];
+let defenders = {}
+
 
 function checkKingAllowedMoves(col, row) {
 
@@ -58,7 +60,7 @@ function getEachEnemyPieceAllowedMoves(pieces, enemyColor) {
 
     let concatedArray = [];
     Object.keys(pieces)?.map(sinlgePiece => {
-        if (pieces[sinlgePiece]?.color !== enemyColor) {
+        if (pieces[sinlgePiece]?.color === enemyColor) {
             // get all the allowed moves for all the enemy pieces ;
             concatedArray = [...concatedArray, ...checkMovesForSinglePiece(pieces[sinlgePiece], sinlgePiece[0], sinlgePiece[1], pieces)];
         }
@@ -67,12 +69,86 @@ function getEachEnemyPieceAllowedMoves(pieces, enemyColor) {
     return filteredArray;
 }
 
+function getFriedlyPiecesAllowedMoves(pieces, enemyColor, attackerCurrentSquare) {
+
+
+    let concatedArrayTow = [];
+    Object.keys(pieces)?.map(sinlgePiece => {
+        if (pieces[sinlgePiece]?.color !== enemyColor) {
+            // get all the allowed moves for all the enemy pieces ;
+            let singlePieceAllowedMoves = checkMovesForSinglePiece(pieces[sinlgePiece], sinlgePiece[0], sinlgePiece[1], pieces);
+            // store the defenders in a new object
+            if (singlePieceAllowedMoves.includes(attackerCurrentSquare)) {
+                defenders = { ...defenders, [pieces[sinlgePiece]?.type]: attackerCurrentSquare }
+            }
+            concatedArrayTow = [...concatedArrayTow, ...singlePieceAllowedMoves];
+        }
+    })
+    let filteredArray = concatedArrayTow?.filter(item => !item.includes('0') && !item.includes('9') && item.length <= 2);
+    return filteredArray;
+}
+
+
+
+const checkIfAttackerCouldBeEaten = (attackerPiece, attackerCurrentSquare, pieces, enemyColor) => {
+
+    // need to determine the attacker type 
+
+    // cehck if the attacker square is in the allowed moves for any of my pieces
+
+    // We have 2 categoryies of attackers --------->>>> queen bishop pawn rook ||| knight
+
+    getFriedlyPiecesAllowedMoves(pieces, enemyColor, attackerCurrentSquare)
+}
+
+
+const checkIfCanAnyPieceBlock = (attackerPiece, attackerCurrentSquare, currentKingSquare, pieces) => {
+
+    //need to check if i can move any of my pieces to be in the attacker allowed moves to block him
+
+    // When want to block ---> the attacker only could be (queen, bishop, pawn, rook)
+
+
+    console.log(currentKingSquare + " " + attackerCurrentSquare);
+
+    //rook attack logic
+    if (Number(attackerCurrentSquare[0]) > Number(currentKingSquare[0]) && Number(attackerCurrentSquare[1]) > Number(currentKingSquare[1])) {
+        console.log('its top right');
+    } else if (Number(attackerCurrentSquare[0]) > Number(currentKingSquare[0]) && Number(attackerCurrentSquare[1]) < Number(currentKingSquare[1])) {
+        console.log('its bottom left');
+        // }else if(Number(attackerCurrentSquare[0]) > Number(currentKingSquare[0]) && Number(attackerCurrentSquare[1]) < Number(currentKingSquare[1])) {
+        console.log('its bottom left');
+    }
+    // else{
+    //     console.log('its to the left');
+    // }
+
+
+    // if(Number(currentKingSquare[1]) < Number(attackerCurrentSquare[1])){
+    //     console.log('its to the top');
+    // }
+
+}
+
+
+
+
+
 
 export const AllowedMovesToEscapeCheckMate = (isCheckMate, attackerPiece, attackerCurrentSquare, currentKingSquare, pieces) => {
 
     // <<<<<<<<<==========================================  1   ==========================================>>>>>>>>>>
 
-    let enemyColor = isCheckMate.white === true ? 'white' : 'black';
+    // rest the variable :
+
+    let resultObject = {};
+
+    let enemyColor;
+    if (isCheckMate.white === true) {
+        enemyColor = 'black';
+    } else if (isCheckMate.black === true) {
+        enemyColor = 'white';
+    }
     let col = currentKingSquare[0];
     let row = currentKingSquare[1];
 
@@ -85,7 +161,6 @@ export const AllowedMovesToEscapeCheckMate = (isCheckMate, attackerPiece, attack
         if (!enemyPiecesAllowedMoves.includes(kingPossibleMove) &&
             pieces[kingPossibleMove]?.color !== enemyColor &&
             !kingPossibleMove.includes('0')) {
-
             kingAllowedMoves.push(kingPossibleMove);
         }
 
@@ -94,12 +169,19 @@ export const AllowedMovesToEscapeCheckMate = (isCheckMate, attackerPiece, attack
     // <<<<<<<<==========================================  2   ==========================================>>>>>>>>>>
 
 
+    checkIfAttackerCouldBeEaten(attackerPiece, attackerCurrentSquare, pieces, enemyColor);
+
+
+    checkIfCanAnyPieceBlock(attackerPiece, attackerCurrentSquare, currentKingSquare, pieces)
+
 
 
 
     let finalArray = Array.from(new Set(kingAllowedMoves))
 
-    return { ...checkMateAllowedMoves, 'king': finalArray }
+    resultObject = { ...checkMateAllowedMoves, 'king': finalArray, ...defenders }
+
+    return resultObject;
 
 
     //TODO: THIS FUNCTION MUST RETURN THE OBJECT OF THE ALLOWED MOVES IN THE CHECK-MATE CASE ;
